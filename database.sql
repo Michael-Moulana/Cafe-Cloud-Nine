4eaa045b85a993104bdaca77b01ed0ddfe489e34
Michael Moulana <100243884+Michael-Moulana@users.noreply.github.com>
2025-06-01 16:39:26 +1000

    Update database.sql

--- database.sql
+++ database.sql
@@ -178,13 +178,13 @@ INSERT INTO user_order(userID, order_date, delivery_addressID, status, total_amo
 (2, '2025-05-31 19:50:04', 2, 'pending', 45.00, 'cash', 'eco-delivery'),
 (3, '2025-05-31 19:51:57', 3, 'pending', 39.00, 'cash', 'standard-delivery');
 
-INSERT INTO order_items(orderID, itemID, quantity, unit_price, total_price) VALUES
-(1, 2, 1, 19.00, 19.00),
-(1, 3, 1, 8.00, 8.00),
-(2, 1, 1, 20.00, 20.00),
-(2, 4, 2, 7.00, 14.00),
-(3, 5, 1, 27.00, 27.00),
-(3, 6, 1, 18.00, 18.00),
-(4, 3, 2, 8.00, 16.00),
-(4, 8, 1, 10.00, 10.00),
-(4, 9, 1, 13.00, 13.00);
+INSERT INTO order_items(orderID, itemID, quantity, unit_price) VALUES
+(1, 2, 1, 19.00),
+(1, 3, 1, 8.00),
+(2, 1, 1, 20.00),
+(2, 4, 2, 7.00),
+(3, 5, 1, 27.00),
+(3, 6, 1, 18.00),
+(4, 3, 2, 8.00),
+(4, 8, 1, 10.00),
+(4, 9, 1, 13.00);
